b08f9718178031b85c4e634e1368ecbd07800de5
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-09-15 19:26:26 +0530

    user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,34 @@
+import { Document, Schema, model } from "mongoose";
+
+interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<User>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = model("User", userSchema);
+
+export default UserModel;
